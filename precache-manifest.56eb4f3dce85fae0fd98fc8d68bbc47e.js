23183e4b843224b3bae20f909b6e4d11a33a5cf4
AkashiTakumi <akataku3025@gmail.com>
2022-07-16 01:44:12 +0900

    react version

---
+++ precache-manifest.56eb4f3dce85fae0fd98fc8d68bbc47e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0c51dd56cdb3b12c4c7",
+    "url": "/static/css/main.8dc82860.chunk.css"
+  },
+  {
+    "revision": "e0c51dd56cdb3b12c4c7",
+    "url": "/static/js/main.e0c51dd5.chunk.js"
+  },
+  {
+    "revision": "b316e8c686d580140adf",
+    "url": "/static/js/1.b316e8c6.chunk.js"
+  },
+  {
+    "revision": "09ff34953d7db0fa2ac6",
+    "url": "/static/js/2.09ff3495.chunk.js"
+  },
+  {
+    "revision": "85e1dd782db34ca903ae",
+    "url": "/static/js/runtime~main.85e1dd78.js"
+  },
+  {
+    "revision": "a23b81c0eb3360cda7008c26173f3090",
+    "url": "/static/media/mypic_1.a23b81c0.JPG"
+  },
+  {
+    "revision": "13eecbd99a1eed2a129323c1648bf98f",
+    "url": "/static/media/mypic_2.13eecbd9.JPG"
+  },
+  {
+    "revision": "523eb5e7355487c6481d0e46694b657a",
+    "url": "/static/media/mypic_3.523eb5e7.JPG"
+  },
+  {
+    "revision": "dc03ca45b4d8bf8cd28cc70151adb2b8",
+    "url": "/static/media/Twitter_icon.dc03ca45.png"
+  },
+  {
+    "revision": "f175eeff6913c0dbc54bb04cb5f59abc",
+    "url": "/static/media/instagram.f175eeff.png"
+  },
+  {
+    "revision": "cc2b4d12e5d28a37aba275dcc3c09f96",
+    "url": "/static/media/sea-ocean_00013.cc2b4d12.jpg"
+  },
+  {
+    "revision": "a7679fc0da0c4bffab83af7d345ea3d7",
+    "url": "/index.html"
+  }
+];
